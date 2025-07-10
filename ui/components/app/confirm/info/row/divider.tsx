83f6044089c9d9adf24f03452c21397df38101a6
christopheschuster <146816752+christopheschuster@users.noreply.github.com>
2025-07-10 08:35:02 +0100

    Create divider.tsx

---
+++ ui/components/app/confirm/info/row/divider.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ConfirmInfoRowDivider: React.FC = () => (
+  <div style={{ padding: '8px 0', margin: '0 -8px' }}>
+    <div style={{ height: 1, backgroundColor: 'var(--color-border-muted)' }} />
+  </div>
+);
