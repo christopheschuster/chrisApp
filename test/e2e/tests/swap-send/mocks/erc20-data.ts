ef9ff0887eea5c5a515e5f4de28d120ae8b5d2a2
christopheschuster <146816752+christopheschuster@users.noreply.github.com>
2025-07-13 07:06:23 +0100

    Create erc20-data.ts

---
+++ test/e2e/tests/swap-send/mocks/erc20-data.ts
@@ -0,0 +1,6 @@
+export const SWAP_SEND_QUOTES_RESPONSE_TST_ETH = [{
+  aggregator: 'ZeroX',
+  aggregatorType: 'AGG',
+  destinationToken: '0x00000000000008d7399155de22a54f7c6aa1f8f6',
+  sourceToken: '6B175474E89E2C3A3FfCacDdE1BeC3cBDaCcAe', // etc
+}]
