473993fea8fc0f5620e92578f787d32432ea03fd
christopheschuster <146816752+christopheschuster@users.noreply.github.com>
2025-07-15 01:42:05 +0100

    Create invalid-custom-network-alert.stories.tsx

---
+++ ui/components/app/alerts/invalid-custom-network-alert/invalid-custom-network-alert.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import InvalidCustomNetworkAlert from './invalid-custom-network-alert';
+import { ALERT_STATE } from '../../../../ducks/alerts';
+
+const store = configureStore([])({
+  invalidCustomNetwork: {
+    state: ALERT_STATE.ERROR,
+    networkName: 'Test Network',
+  },
+});
+
+const meta: Meta<typeof InvalidCustomNetworkAlert> = {
+  title: 'Components/App/Alerts/InvalidCustomNetworkAlert',
+  component: InvalidCustomNetworkAlert,
+  decorators: [(Story) => <Provider store={store}><Story /></Provider>],
+  argTypes: { history: { control: 'object' } },
+  args: { history: { push: () => {} } },
+};
+
+export default meta;
+type Story = StoryObj<typeof InvalidCustomNetworkAlert>;
+
+export const DefaultStory: Story = {
+  render: (args) => <InvalidCustomNetworkAlert {...args} />,
+};
+DefaultStory.storyName = 'Default';
